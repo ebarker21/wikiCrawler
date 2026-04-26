1217582a323fca52ab88a1d22ab54eabe5d6e2f7
Eric <ericbarker21@gmail.com>
2026-04-26 17:57:27 -0400

    starting wiki-crawler

---
+++ pytravel.py
@@ -0,0 +1,44 @@
+import sqlite3
+import requests
+import random
+from lxml import html
+
+visited = set()
+
+def webCrawl(url):
+    headers = {'User-Agent': 'MyUserAgent/1.0'}
+    response = requests.get(url, headers=headers)
+
+    tree = html.fromstring(response.content)
+
+    titles = tree.xpath('//h1[@*]//text()')
+    links = tree.xpath('//a/@href')
+    linkTitles = tree.xpath('//a/@title')
+
+    return {
+        "title": titles[0],
+        "links": links,
+        "linkTitles": linkTitles
+        }
+
+currentURL = 'https://en.wikipedia.org/wiki/Linux'
+while True:
+    pageData = webCrawl(currentURL)
+    
+    visited.add(currentURL)
+    
+    print(f'Currently at {pageData['title']}')
+    
+    wikiLinks = [
+    l for l in pageData['links']
+    if l.startswith('/wiki/') 
+    and 'Main_Page' not in l
+    and ('https://en.wikipedia.org' + l) not in visited
+    ]
+
+    if not wikiLinks:
+        print("Reached a dead end..?")
+        break
+    randomIndex = random.randint(0, len(wikiLinks)-1)
+    currentURL = 'https://en.wikipedia.org' + wikiLinks[randomIndex]
+
