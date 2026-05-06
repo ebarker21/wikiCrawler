f173f3a6fecad227e18e83c7254b2970c8909624
Eric <ericbarker21@gmail.com>
2026-05-06 15:14:58 -0400

    moves functions to single file

---
+++ functions.py
@@ -0,0 +1,67 @@
+from pyvis.network import Network
+from db import insert_page, insert_link, get_pages, get_links
+from lxml import html
+import requests
+
+web = Network()
+visited = []
+
+
+def webCrawl(url):
+    headers = {'User-Agent': 'MyUserAgent/1.0'}
+    response = requests.get(url, headers=headers)
+
+    tree = html.fromstring(response.content)
+
+    title = tree.xpath('//h1[@id="firstHeading"]//text()')
+    links = tree.xpath('//a/@href')
+    paragraphs = tree.xpath('//div[@id="mw-content-text"]//p[not(@class)][1]')
+    summary = paragraphs[0].text_content().strip() if paragraphs else ""
+
+    return {
+        "title": title[0],
+        "links": links,
+        "summary": summary
+        }
+
+
+def create_web():
+    for page in get_pages():
+        web.add_node(value=page[0], n_id=page[1], label=page[2], title=page[3])
+        print(f"Added node: {page[2]}")
+    for link in get_links():
+        web.add_edge(link[0], link[1])
+        print(f"Added edge: {link[0]} -> {link[1]}")
+
+
+def recursive_branch(currentURL, depth):
+    if depth > 5:  # Limit the depth of the recursion
+        return
+
+    pageData = webCrawl(currentURL)
+    print(f'Currently at {pageData["title"]} (depth: {depth})')
+    visited.append(currentURL)
+    insert_page(currentURL, pageData['title'], pageData['summary'])
+
+    wikiLinks = [
+        l for l in pageData['links']
+        if l.startswith('/wiki/')
+        and 'Main_Page' not in l
+        and 'Help:' not in l
+        and 'Template:' not in l
+        and 'Wikipedia:' not in l
+        and 'Special:' not in l
+        and 'File:' not in l
+        and 'Category' not in l
+        and 'Talk:' not in l
+        and ('https://en.wikipedia.org' + l) not in visited
+    ]
+
+    for link in wikiLinks:
+        for visited_link in visited:
+            if visited_link == 'https://en.wikipedia.org' + link:
+                insert_link(currentURL, 'https://en.wikipedia.org' + link)
+                print ("found connection!")
+        newURL = 'https://en.wikipedia.org' + link
+        insert_link(currentURL, newURL)
+        recursive_branch(newURL, depth + 1)
--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import requests
-import random
-from lxml import html
-from db import init_db, insert_page, insert_link, get_links, get_pages
+from db import init_db
 from network import create_web
+import recursive_branch
 
 currentURL = 'https://en.wikipedia.org/wiki/Linux'
 visited = []
@@ -12,7 +10,9 @@ network_limit = 100
 depth = 0
 
 while True:
-
+    recursive_branch.recursive_branch(currentURL, depth)
+    create_web()
+    break
 #     pageData = webCrawl(currentURL)
         
 #     print(f'Currently at {pageData['title']}')
@@ -49,57 +49,9 @@ while True:
         create_web()
         break
     
-    newURL = 'https://en.wikipedia.org' + random.choice(wikiLinks)
-    insert_link(currentURL, newURL)
-    currentURL = newURL
+    # newURL = 'https://en.wikipedia.org' + random.choice(wikiLinks)
+    # insert_link(currentURL, newURL)
+    # currentURL = newURL
 
 
     
-def webCrawl(url):
-    headers = {'User-Agent': 'MyUserAgent/1.0'}
-    response = requests.get(url, headers=headers)
-
-    tree = html.fromstring(response.content)
-
-    title = tree.xpath('//h1[@id="firstHeading"]//text()')
-    links = tree.xpath('//a/@href')
-    paragraphs = tree.xpath('//div[@id="mw-content-text"]//p[not(@class)][1]')
-    summary = paragraphs[0].text_content().strip() if paragraphs else ""
-
-    return {
-        "title": title[0],
-        "links": links,
-        "summary": summary
-        }
-
-def recursive_branch(currentURL, depth):
-    if depth > 5:  # Limit the depth of the recursion
-        return
-
-    pageData = webCrawl(currentURL)
-    print(f'Currently at {pageData["title"]} (depth: {depth})')
-    visited.append(currentURL)
-    insert_page(currentURL, pageData['title'], pageData['summary'])
-
-    wikiLinks = [
-        l for l in pageData['links']
-        if l.startswith('/wiki/')
-        and 'Main_Page' not in l
-        and 'Help:' not in l
-        and 'Template:' not in l
-        and 'Wikipedia:' not in l
-        and 'Special:' not in l
-        and 'File:' not in l
-        and 'Category' not in l
-        and 'Talk:' not in l
-        and ('https://en.wikipedia.org' + l) not in visited
-    ]
-
-    for link in wikiLinks:
-        for visited_link in visited:
-            if visited_link == 'https://en.wikipedia.org' + link:
-                insert_link(currentURL, 'https://en.wikipedia.org' + link)
-                print ("found connection!")
-        newURL = 'https://en.wikipedia.org' + link
-        insert_link(currentURL, newURL)
-        recursive_branch(newURL, depth + 1)
